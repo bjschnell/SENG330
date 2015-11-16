96681a056aed160238bf8c61ed98dcc6e26b183d
Brady <Bradyschnell@gmail.com>
2015-11-15 22:53:59 -0800

    Prototypes working for user input

--- SENGass2/main.cpp
+++ SENGass2/main.cpp
@@ -5,108 +5,139 @@ using namespace std;
 
 class Prototype {
 protected:
-    std::string type;
-    int value;
+    string type;
+    string name;
 
 public:
     virtual Prototype* clone() = 0;
 
     // getter's for type
-    std::string getType() {
+    string getType() {
         return type;
     }
     // Getter for value
-    int getValue() {
-        return value;
+    string getName() {
+        return name;
     }
 
+    void setType(string type) {
+        this->type = type;
+    }
+
+    void setName(string Name) {
+        this->name = Name;
+    }
 };
 // Machine1 class clones from prototype
 class Machinetype1 : public Prototype
 {
 public:
     // Init
-    Machinetype1(int ID) {
-        type = "Machine Type 1";
-        value = ID;
+    Machinetype1(string utype, string ID) {
+        type = utype;
+        name = ID;
     }
     // clones this object
     Prototype* clone() {
         return new Machinetype1(*this);
+    }
+
+    // GETTERS
+    string getType() {
+        return type;
+    }
+
+    string getName() {
+        return name;
+    }
+
+    // SETTERS
+    void setName(string Name) {
+        this->name = Name;
+    }
+
+    void setType(string type) {
+        this->type = type;
     }
 };
 
 class Machinetype2 : public Prototype
 {
 public:
-    Machinetype2(int ID) {
-        type = "Machine Type 2";
-        value = ID;
+    // Init
+    Machinetype2(string utype, string ID) {
+        type = utype;
+        name = ID;
     }
-
+    // Clone for this machine
     Prototype* clone() {
         return new Machinetype2(*this);
     }
-};
-
-class Factory {
-    static Prototype* machine1;
-    static Prototype* machine2;
-    static Prototype* machine3;
-    static Prototype* machine4;
-
-public:
-    // Initialize the factory with 4 machines
-    static void init() {
-        machine1 = new Machinetype1(1);
-        machine2 = new Machinetype1(2);
-        machine3 = new Machinetype2(3);
-        machine4 = new Machinetype2(4);
-    }
-    // getters using clones of the initalized machines
-    static Prototype* getMachine1() {
-        return machine1->clone();
+    // GETTERS
+    string getType() {
+        return type;
     }
 
-    static Prototype* getMachine2() {
-        return machine2->clone();
+    string getName() {
+        return name;
+    }
+    // SETTERS
+    void setName(string Name) {
+        this->name = Name;
     }
 
-    static Prototype* getMachine3() {
-        return machine3->clone();
-    }
-
-    static Prototype* getMachine4() {
-        return machine4->clone();
+    void setType(string type) {
+        this->type = type;
     }
 };
 
-Prototype* Factory::machine1 = 0;
-Prototype* Factory::machine2 = 0;
-Prototype* Factory::machine3 = 0;
-Prototype* Factory::machine4 = 0;
-
+Prototype* machine1 = 0;
+Prototype* machine2 = 0;
 
 int main()
 {
-    Factory::init();
-    Prototype* demo;
+    Prototype* demo[10];
+    string machinetype[10];
+    string id[10];
+    int i = 0;
+    int index = 0;
 
-    // all the following objects should be created via cloning of the prototype
-    demo = Factory::getMachine1();
-    cout << "Machine Type:" << demo->getType() << " ID: " << demo->getValue() << endl;
+    // Get users input in a semi-logical way
+    while (machinetype[index] != "-1" || id[index] != "-1"){
+        if (i > 1) {
+            index++;
+        }
+        cout << "enter the machine type (1 or 2) and press return (-1 to quit)" << endl;
+        cin >> machinetype[i];
+        if (machinetype[i] == "-1") {
+            break;
+        }
+        cout << "enter the id of the machine and press return (-1 to quit)" << endl;
+        cin >> id[i];
+        if (id[i] == "-1") {
+            break;
+        }
+        i++;
+    }
 
+    // Declaration of prototypes for cloning
+    static Prototype* machine1 = new Machinetype1("machine 1", "1");
+    static Prototype* machine2 = new Machinetype2("machine 2", "2");
 
-    demo = Factory::getMachine2();
-    cout << "Machine Type:" << demo->getType() << " ID: " << demo->getValue() << endl;
+    // Allocate new machines via cloning the original prototypes. YAY
+    for(int j = 0; j <= i; j++){
+        if (machinetype[j] == "1") {
+            demo[j] = machine1->clone();
+        } else {
+            demo[j] = machine2->clone();
+        }
+        demo[j]->setName(id[j]);
+    }
 
-
-    demo = Factory::getMachine3();
-    cout << "Machine Type:" << demo->getType() << " ID: " << demo->getValue() << endl;
-
-
-    demo = Factory::getMachine4();
-    cout << "Machine Type:" << demo->getType() << " ID: " << demo->getValue() << endl;
+    // Prints out the machines info
+    for (int j = 0; j <= i; j++) {
+        cout << "Machine type: " << demo[j]->getType() << " Machine ID: " << demo[j]->getName() << endl;
+    }
 
 }
 
